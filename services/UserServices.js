5389979a19ceac10f5edc3225956bc9bfc06f016
luan048 <luanmonteiropedrosa@gmail.com>
2025-01-25 11:20:26 -0300

    Created Services

---
+++ services/UserServices.js
@@ -0,0 +1,49 @@
+import bcrypt from 'bcrypt'
+import {User} from '../models/UserModel.js'
+
+export class AuthenticationUser {
+    constructor(database) {
+        this.database = database
+    }
+
+    findAll() {
+        return this.database.findAll()
+    }
+
+    register(name, email, password) {
+        const userExistis = this.database.findByEmail(email)
+
+        if(userExistis) throw new Error("E-mail já cadastrado!")
+
+        const newUser = new User({name, email, password})
+        newUser.password = bcrypt.hashSync(newUser.password, 10)
+
+        this.database.save(newUser)
+        return newUser
+    }
+
+    login(email, password) {
+        const user = this.database.findByEmail(email)
+
+        if(!user) throw new Error("Usuário não cadastrado")
+        if(!bcrypt.compareSync(password, user.password)) throw new Error("Senha incorreta!")
+        
+        return {
+            message: "Login realizado com sucesso",
+            user: {...user, password: undefined}
+        }
+    }
+
+    updatePassword(email, lastPassword, newPassword) {
+        const userExistis = this.database.findByEmail(email)
+
+        if(!userExistis) throw new Error("Email não cadastrado!")
+
+        if(!bcrypt.compareSync(lastPassword, userExistis.password)) throw new Error("Senha incorreta!")
+        
+        userExistis.password = bcrypt.hashSync(newPassword, 10)
+
+        this.database.updatePassword(userExistis)
+        return userExistis
+    }
+}
