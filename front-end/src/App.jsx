8486783f7edfdef3bf05fe741d33077622dd1c6a
luan048 <luanmonteiropedrosa@gmail.com>
2025-01-30 10:52:47 -0300

    Started Style in Login Page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -9,18 +9,15 @@ function App() {
                 <p className="entrar">Entrar</p>
 
                 <div className="input-divs">
-                    <p className="email">Email</p>
-                    <input className="input-email" type="text" />
-
-                    <p className="password">Password</p>
-                    <input className="input-password" type="password" />
+                    <i class="fa-regular fa-user"></i><input className="input-email" type="text" placeholder="Email"/>
+                    <i class="fa-solid fa-lock"></i><input className="input-password" type="password" placeholder="Senha"/>
                 </div>
 
-                <button className="login-button">Faça Login</button>
+                <a className="forgot-password" href="">Esqueci Senha</a>
 
                 <div className="extra-buttons">
                     <button className="signup-button">Cadastrar</button>
-                    <a className="forgot-password" href="">Esqueci Senha</a>
+                <button className="login-button">Faça Login</button>
                 </div>
             </div>
         </>
