d17467a275418f78cae8738a93b63855beb598ca
luan048 <luanmonteiropedrosa@gmail.com>
2025-01-31 10:42:42 -0300

    Add Functions in Input Password

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,23 +1,44 @@
 import React from "react";
+import {useState} from 'react'
 
 import './App.css'
 
 function App() {
+
+    const [isVisible, setIsVisible] = useState(false)
+
+    function toggleVisibility() {
+        setIsVisible(!isVisible)
+    }
+
     return (
         <>
             <div className="general-div">
                 <p className="entrar">Entrar</p>
 
                 <div className="input-divs">
-                    <i class="fa-regular fa-user"></i><input className="input-email" type="text" placeholder="Email"/>
-                    <i class="fa-solid fa-lock"></i><input className="input-password" type="password" placeholder="Senha"/>
+
+                    <div className="div-input-email">
+                        <span className="span-icon-user"><i className="fa-regular fa-user"></i></span><input className="input-email" type="text" placeholder="Email"/>
+                    </div>
+            
+                    <div className="div-input-password">
+                        <span className="span-icon-password"><i className="fa-solid fa-lock"></i></span>
+                        <input className="input-password" type={isVisible ? "text" : "password"} placeholder="Senha"/>
+                        <i
+                            className={`fa-solid ${isVisible ? "fa-eye" : "fa-eye-slash"}`}
+                            onClick={toggleVisibility}
+                            style={{cursor: "pointer", marginLeft: "8px"}}
+                        ></i>
+                    </div>
+
                 </div>
 
                 <a className="forgot-password" href="">Esqueci Senha</a>
 
                 <div className="extra-buttons">
                     <button className="signup-button">Cadastrar</button>
-                <button className="login-button">Faça Login</button>
+                    <button className="login-button">Faça Login</button>
                 </div>
             </div>
         </>
