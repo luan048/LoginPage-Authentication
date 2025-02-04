75a209e4051ae02e84cd33b139b129adec71b54e
luan048 <luanmonteiropedrosa@gmail.com>
2025-02-04 15:14:19 -0300

    Add Forget Password Router

---
+++ front-end/ForgetPass/ForgetPass.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './ForgetPass.css'
+
+function ForgetPass() {
+    return (
+        <div>
+            <h1>TESTE FORGET PASSWORD</h1>
+        </div>
+    )
+}
+
+export default ForgetPass
--- front-end/src/Main.jsx
+++ front-end/src/Main.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
+// Routes
 import App from "./App.jsx";
+import ForgetPass from "../ForgetPass/ForgetPass.jsx";
+// Fim Routes
+
+const routers = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    },
+
+    {
+        path: "/forgetpass",
+        element: <ForgetPass />
+    }
+])
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={routers}/>
     </StrictMode>
 )
