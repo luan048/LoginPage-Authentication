16eece3516e77353ad1afbcfd69e6bec06b5e6a5
luan048 <luanmonteiropedrosa@gmail.com>
2025-02-04 21:38:58 -0300

    Add Routes in the buttons

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {useState} from 'react'
+import { Link } from "react-router-dom"; 
 
 import './App.css'
 
@@ -19,23 +20,28 @@ function App() {
                 <div className="input-divs">
 
                     <div className="div-input-email">
-                        <span className="span-icon-user"><i className="fa-regular fa-user"></i></span><input className="input-email" type="text" placeholder="Email"/>
+                        <span className="span-icon-user"><i className="fa-regular fa-user" /></span><input className="input-email" type="text" placeholder="Email"/>
                     </div>
             
                     <div className="div-input-password">
-                        <span className="span-icon-password"><i className="fa-solid fa-lock"></i></span>
+                        <span className="span-icon-password"><i className="fa-solid fa-lock" /></span>
 
-                        <input className="input-password" type={isVisible ? "text" : "password"} placeholder="Senha"/>
-                        <i className={`fa-solid ${isVisible ? "fa-eye" : "fa-eye-slash"}`} onClick={toggleVisibility}></i>
+                        <input className="input-password" type={isVisible ? "text" : "password"} placeholder="Senha" />
+                        <i className={`fa-solid ${isVisible ? "fa-eye" : "fa-eye-slash"}`} onClick={toggleVisibility} />
                     </div>
 
                 </div>
 
-                <a className="forgot-password" href="">Esqueci Senha</a>
+                {/* Link Forget-Password */}
+                <Link className="forgot-password" to={"/forgetpass"}>Esqueci Senha</Link>
+                {/* Fim Link Forget-Password */}
 
                 <div className="extra-buttons">
                     <button className="signup-button">Cadastrar</button>
-                    <button className="login-button">Faça Login</button>
+
+                    {/* Button to Sign In */}
+                    <button className="login-button"><Link to={"/signup"} style={{textDecoration: "none", color: "inherit"}}>Faça Login</Link></button>
+                    {/* Fim Button to Sign In */}
                 </div>
             </div>
         </>
