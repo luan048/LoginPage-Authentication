a51ce741020a2bd83a6e283ee55689cb506d4aec
luan048 <luanmonteiropedrosa@gmail.com>
2025-02-04 22:26:52 -0300

    Made Forget Password Front-end

--- front-end/ForgetPass/ForgetPass.jsx
+++ front-end/ForgetPass/ForgetPass.jsx
@@ -4,8 +4,18 @@ import './ForgetPass.css'
 
 function ForgetPass() {
     return (
-        <div>
-            <h1>TESTE FORGET PASSWORD</h1>
+        <div className="general-div-fgpass">
+            <p className="title">Criar Nova Senha</p>
+
+            <div className="div-inputs-fgpass">
+                <input className="email-fgpass" type="text" placeholder="Email"/>
+                <input className="lastP-fgpass" type="text" placeholder="Senha Antiga"/>
+                <input className="newP-fgpass" type="text" placeholder="Nova Senha"/>
+            </div>
+
+            <div className="div-button-fgpass">
+                <button className="button-fgpass">Redefinir Senha</button>
+            </div>
         </div>
     )
 }
