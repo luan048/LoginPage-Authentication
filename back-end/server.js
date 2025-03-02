4e8d37fc9c56b9cc62fb896a3507f752a03a32c1
luan048 <luanmonteiropedrosa@gmail.com>
2025-03-02 11:46:49 -0300

    Add Cors Politics

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import {routers} from './routers/UserRouter.js'
+import cors from 'cors'
 
 const server = express()
 const port = 8000
 
+server.use(cors())
 server.use(express.json())
 server.use(routers)
 
