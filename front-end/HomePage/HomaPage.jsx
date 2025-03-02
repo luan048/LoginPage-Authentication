3534796b3c905fc499c568b246c86b941c67930e
luan048 <luanmonteiropedrosa@gmail.com>
2025-03-02 09:56:37 -0300

    Created HomePage

---
+++ front-end/HomePage/HomaPage.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './HomePage.css'
+
+function HomePage() {
+    return (
+        <div className="general-div-homepage">
+            <h1>Seja bem vindo(a)!</h1>
+            <p>Sistema feito para simular um ambiente de autenticação.</p>
+            <p>Nesse sistema é possivel criar conta, fazer login, e recuperar senha</p>
+        </div>
+    )
+}
+
+export default HomePage
